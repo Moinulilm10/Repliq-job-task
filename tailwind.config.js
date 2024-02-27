b8b3adb1e6adc7cb20072d07ca5de0f504670d46
Moinul Islam <moinulilm10@gmail.com>
2024-02-27 23:27:07 +0600

    color config add in tailwind css config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5C6AC4",
+        primaryBg: "#E7E9F6",
+        greyBg: "#84919C",
+        secondary: "#3674D9",
+        secondaryBg: "#E1EAF9",
+        error: "red",
+        errorBg: "#FADEDD",
+      },
+    },
   },
   plugins: [],
 };
-
